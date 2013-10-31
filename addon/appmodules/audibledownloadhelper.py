1445dd1a998dd6e1c705b440c6d4b5251d731dc9
David Parduhn <xkill85@gmx.net>
2013-10-31 09:35:08 +0100

    Now I'm sing the add-on template to build the add-on.

---
+++ addon/appmodules/audibledownloadhelper.py
@@ -0,0 +1,17 @@
+#appModules/audibledownloadhelper.py
+#A part of NonVisual Desktop Access (NVDA)
+#Copyright (C) 2006-2012 NVDA Contributors
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+import appModuleHandler
+import api
+import controlTypes
+from NVDAObjects.IAccessible import IAccessible
+class audibledownloadmanagerlistitem(IAccessible):
+	def _get_name(self):
+		if self.displayText:
+			return(self.displayText)
+class AppModule(appModuleHandler.AppModule):
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.role == controlTypes.ROLE_LISTITEM:
+			clsList.insert(0,audibledownloadmanagerlistitem)
---
+++ buildVars.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon-name" : "audibledownloadmanager",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon-summary" : _("audible download manager"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon-description" : _("""this add-on improves the accessibility of the audible download manager.
+Instead of displaying the full paths of the .adh files in the downloads list, meta infos are displayed (such as the download status)."""),
+	# version
+	"addon-version" : "1.0",
+	# Author(s)
+	"addon-author" : u"David Parduhn <xkill85@gmx.net>",
+	# URL for the add-on documentation support
+	"addon-url" : None
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = []
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
