3922dc02428fb64630be54faedf0866e8dde9481
David Parduhn <xkill85@gmx.net>
2021-02-03 12:53:51 +0100

    1.3: - das Anwendungsmodul für Wiundows 10 ermöglicht das Aufrufen des Kontextmenüs zu einem Hörbuch mit der Kontextmenütaste oder mit der Tastenkombination Umschalt+fF10

--- addon/appmodules/audiblert_windowsphone.py
+++ addon/appmodules/audiblert_windowsphone.py
@@ -1,16 +1,49 @@
 import appModuleHandler
 import controlTypes
 from NVDAObjects.UIA import UIA, ListItem
-class audiblegroup(UIA):
+import mouseHandler
+import winUser
+
+class audibleunlabeledcontrol (UIA):
 	def _get_name(self):
 		return self.firstChild.name
 class audiblelistitem(ListItem):
+	__gestures = {
+		"kb:enter": "linksklick",
+		"kb:numpadEnter": "linksklick",
+		"kb:shift+f10": "rechtsklick",
+		"kb:applications": "rechtsklick"
+	}
+	def script_linksklick(self, gesture):
+		x = self.location.center.x
+		y = self.location.center.y
+		winUser.setCursorPos(x,y)
+		mouseHandler.executeMouseMoveEvent(x,y)
+		mouseHandler.executeMouseEvent(winUser.MOUSEEVENTF_LEFTDOWN,0,0)
+		mouseHandler.executeMouseEvent(winUser.MOUSEEVENTF_LEFTUP,0,0)
+
+	def script_rechtsklick(self, gesture):
+		x = self.location.center.x
+		y = self.location.center.y
+		winUser.setCursorPos(x,y)
+		mouseHandler.executeMouseMoveEvent(x,y)
+		mouseHandler.executeMouseEvent(winUser.MOUSEEVENTF_RIGHTDOWN,0,0)
+		mouseHandler.executeMouseEvent(winUser.MOUSEEVENTF_RIGHTUP,0,0)
+
+
 	def _get_name(self):
-		return self.firstChild.name
+		l = list()
+		for x in self.children:
+			s = x.name
+			if s != "" and x.role == controlTypes.ROLE_STATICTEXT:
+				l.append(s)
+		return '; '.join(l)
 
 class AppModule(appModuleHandler.AppModule):
 	def chooseNVDAObjectOverlayClasses(self, obj, clslist):
 		if obj.role == controlTypes.ROLE_GROUPING:
-			clslist.insert(0,audiblegroup)
-		elif obj.role == controlTypes.ROLE_LISTITEM and obj.name == "":
+			clslist.insert(0,audibleunlabeledcontrol)
+		elif obj.role == controlTypes.ROLE_BUTTON and obj.name == "":
+			clslist.insert(0,audibleunlabeledcontrol)
+		elif obj.role == controlTypes.ROLE_LISTITEM:
 			clslist.insert(0,audiblelistitem)
