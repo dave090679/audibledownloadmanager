97aaa2ee9eaf10feafa8f8ebf8a43e6c7489190c
David Parduhn <xkill85@gmx.net>
2023-04-14 17:07:44 +0200

    update to nvda 2023.1

---
+++ release.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+# Copyright (C) 2019 Larry Wang <larry.wang.801@gmail.com>
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+import buildVars
+import subprocess
+name  = buildVars.addon_info["addon_name"]
+version = buildVars.addon_info["addon_version"]
+subprocess.call([
+	"git",
+	"tag",
+	'-m',
+	'"{0}"'.format(version),
+	version
+])
+subprocess.call([
+	"git", "push", "--tags"
+])
+subprocess.call([
+	"gh", "release", "create", version, "--generate-notes", name+"-"+version+".nvda-addon"
+])
